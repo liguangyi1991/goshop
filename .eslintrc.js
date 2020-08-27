e78b3cc49ce0a42d48c05f04bfa713297d016d5c
liguangyi <liguangyi@gaosiedu.com>
2020-08-27 12:09:41 +0800

    提交项目

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 0,
+    'comma-dangle': [1, {
+      objects: 'always',
+      arrays: 'ignore',
+      imports: 'ignore',
+      exports: 'ignore',
+      functions: 'ignore',
+    }],
+  }
+}
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Msite from '../pages/MSite/Msite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
+
+Vue.use(VueRouter)
+const routes = [
+  {
+    path: '/msite',
+    component: Msite,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path: '/search',
+    component: Search,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path: '/order',
+    component: Order,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path: '/profile',
+    component: Profile,
+    meta: {
+      showFooter: true
+    }
+  },
+  {
+    path: '/',
+    redirect: '/msite',
+  },
+  {
+    path: '/login',
+    component: Login,
+  },
+]
+const router = new VueRouter({
+  routes
+})
+
+export default router
